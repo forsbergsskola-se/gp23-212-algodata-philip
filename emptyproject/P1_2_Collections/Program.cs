3edb3921ad99c558986b2193c61cc29a7644a147
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2024-05-16 15:16:33 +0200

    Complete P1_2_Collections (all by myself!)

---
+++ emptyproject/P1_2_Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+List<int> list = new List<int>();
+list.Add(137);
+list.Add(1000);
+list.Add(-200);
+
+for (int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i]);
+}
+
+ArrayList arrayList = new ArrayList();
+arrayList.Add(true);
+arrayList.Add("Forsbergs");
+arrayList.Add('a');
+arrayList.Add(1000);
+arrayList.Add(.12f);
+
+for (int i = 0; i < arrayList.Count; i++)
+{
+    Console.WriteLine(arrayList[i]);
+}
