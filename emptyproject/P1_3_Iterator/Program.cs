e8d20a7126dfc70dd66cd9b114e4f0eb23a793bb
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2024-05-16 15:36:54 +0200

    Keepin goin

---
+++ emptyproject/P1_3_Iterator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+List<int> list = new List<int>();
+list.Add(1);
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(5);
+
+IEnumerable<int> enumerablelist = list;
+
+var i = 0;
+while(i  < list.Count)
+{
+    Console.WriteLine(list[i]);
+    ++i;
+}
