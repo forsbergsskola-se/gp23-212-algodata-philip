c80493b2a428a0a588b4afd4f2da636c6dc61d15
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2024-05-16 16:50:10 +0200

    Got stuck, but I'm getting somewhere.

--- emptyproject/P1_3_Iterator/Program.cs
+++ emptyproject/P1_3_Iterator/Program.cs
@@ -1,17 +1,30 @@
-﻿using System.Collections;
-
-List<int> list = new List<int>();
-list.Add(1);
-list.Add(1);
-list.Add(2);
-list.Add(3);
-list.Add(5);
-
-IEnumerable<int> enumerablelist = list;
-
-var i = 0;
-while(i  < list.Count)
+﻿namespace TurboCollections
 {
-    Console.WriteLine(list[i]);
-    ++i;
+    public static class ListClass
+    {
+        public static List<int> list = new List<int>();
+
+        static ListClass()
+        {
+            list.Add(1);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(5);
+        }
+
+        public static void Main(string[] args)
+        {
+            IEnumerable<int> enumerablelist = list;
+
+            var i = 0;
+            while (i < list.Count)
+            {
+                Console.WriteLine(list[i]);
+                ++i;
+            }
+
+            TurboMaths.GetEvenNumbers
+        }
+    }
 }
--- emptyproject/TurboCollections/Class1.cs
+++ emptyproject/TurboCollections/Class1.cs
@@ -6,4 +6,27 @@ public static class TurboMaths
     {
         Console.WriteLine($"Hello, I'm {typeof(TurboMaths).FullName}");
     }
+    
+    public static IEnumerable<int> GetEvenNumbers(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                yield return list[i];
+            }
+        }
+    }
+
+    public static IEnumerable<int> GetEvenNumbersList(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                list.Add(i);
+            }
+        }
+        return list;
+    }
 }
